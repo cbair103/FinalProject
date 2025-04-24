e16b422d93b3eb5d9a449e846aaa16a363d8bfaf
cbair103 <cbair211@gmail.com>
2025-04-23 19:46:34 -0500

    Initial Forgot to Create Repo at Beginning

--- comic_book_database.sql
+++ comic_book_database.sql
@@ -1,4 +1,16 @@
-DROP DATABASE IF EXISTS comic_book_database;
+DROP DATABASE IF EXISTS comic_books;
 
-CREATE DATABASE comic_book_database;
-USE comic_book_database;
+CREATE DATABASE comic_books;
+
+USE comic_books;
+
+CREATE TABLE comicBooks{
+	Publisher VarChar(100),
+    Author VarChar(100),
+    Illustrator VarChar(100),
+    Issue VarChar(50),
+    Variant VarChar(50),
+    Location VarChar(100),
+    Price int
+
+};
