950847f7a796e124b3c139fe45d8a15b0f06e0c3
cbair103 <cbair211@gmail.com>
2025-05-06 08:28:14 -0500

    Modified context to connect to use jdbc

--- comic_book_database.sql
+++ comic_book_database.sql
@@ -4,13 +4,32 @@ CREATE DATABASE comic_books;
 
 USE comic_books;
 
-CREATE TABLE comicBooks{
+CREATE TABLE comicBooks(
+	Title VarChar(100),
 	Publisher VarChar(100),
     Author VarChar(100),
     Illustrator VarChar(100),
-    Issue VarChar(50),
-    Variant VarChar(50),
+    Issue int,
     Location VarChar(100),
     Price int
 
-};
+);
+
+INSERT INTO comicBooks (Title, Publisher, Author, Illustrator, Issue, Location, Price)
+	VALUES("The Amazing Spider-Man", "Marvel Comics", "Stan Lee", "Jack Kirby, Steve Ditko", 1, "The Android's Dungeon & Baseball Card Shop", 336000);
+    
+INSERT INTO comicBooks (Title, Publisher, Author, Illustrator, Issue, Location, Price)
+	VALUES("X-Nation 2099", "Marvel Comics", "Terry Kavanagh and Ben Raab", "Eric Battle", 4, "The Comic Center of Pasadena", 12.99);
+    
+INSERT INTO comicBooks (Title, Publisher, Author, Illustrator, Issue, Location, Price)
+	VALUES("Batman", "DC Comics", "Doug Moench", "Don Newton", 368, "The Comic Center of Pasadena", 125.00);
+    
+INSERT INTO comicBooks (Title, Publisher, Author, Illustrator, Issue, Location, Price)
+	VALUES("Green Lantern: The Silver Age", "DC Comics", "Gardner Fox and John Broome", "Joe Giella", 3, "The Android's Dungeon & Baseball Card Shop", 32.98);
+    
+INSERT INTO comicBooks (Title, Publisher, Author, Illustrator, Issue, Location, Price)
+	VALUES("Adventures of Superman", "DC Comics", "Marv Wolfman", "Jerry Ordway", 432, "The Comic Center of Pasadena", 5.99);
+
+INSERT INTO comicBooks (Title, Publisher, Author, Illustrator, Issue, Location, Price)
+	VALUES("Invincible Ironman", "Marvel Comics", "Matt Fraction", "Jamie Mckelvie", 32, "The Android's Dungeon & Baseball Card Shop", 4.99);
+    
